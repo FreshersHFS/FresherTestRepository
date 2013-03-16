22f16aa39d55d6f63707168130adeeae2fa84638
Sushama <sushama@headfitted.com>
2013-03-15 20:22:25 -0700

    Project is added to create employee

---
+++ wcfCreate/wcfCreate/IService1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace wcfCreate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string CreateEmployee(Employee Emp);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+   
+    [DataContract]
+     public class Employee
+    {
+        int empid;
+        string fname, lname, dob, sex;
+
+
+        [DataMember]
+        public int Empid
+        {
+            get { return empid; }
+            set { empid = value; }
+        }
+
+        [DataMember]
+        public string Fname
+        {
+            get { return fname; }
+            set { fname = value; }
+        }
+
+        [DataMember]
+        public string Lname
+        {
+            get { return lname; }
+            set { lname = value; }
+        }
+
+        [DataMember]
+        public string Dob
+        {
+            get { return dob; }
+            set { dob = value; }
+        }
+
+        [DataMember]
+        public string Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+    }
+}
---
+++ wcfCreate/wcfCreate/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace wcfCreate
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+        
+    public class Service1 : IService1
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=HFSTRSRV2\SQLEXPRESS;Initial Catalog=Sushama;User ID=sa;Password=p@ssw0rd000");
+        SqlCommand cmd = new SqlCommand();
+        Employee Emp = new Employee();
+              [WebInvoke(Method = "POST", UriTemplate = "CreateEmployee", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Bare)]
+
+        public string CreateEmployee(Employee Emp)
+        {
+            string msg;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Employee values(" + Emp.Empid + ",'" + Emp.Fname + "','" + Emp.Lname + "','" + Emp.Dob + "','" + Emp.Sex + "')", con);
+             cmd.ExecuteNonQuery();
+
+              msg = " Details inserted successfully";
+           
+
+            return msg;
+        }
+
+    }
+}
